21a283d6eab4b98461f77802876959d0b29da1f1
Ryohei Ikegami <iofg2100@gmail.com>
2022-09-02 13:47:54 +0900

    Allow longer lines

--- ui-src/format.ts
+++ ui-src/format.ts
@@ -1,9 +1,15 @@
+import type { Options } from "prettier";
 import prettier from "prettier/standalone";
 import parserBabel from "prettier/parser-babel";
 import parserHtml from "prettier/parser-html";
 
+const commonOptions: Options = {
+  printWidth: 1000,
+};
+
 export function formatJS(value: string): string {
   return prettier.format(value, {
+    ...commonOptions,
     parser: "babel",
     plugins: [parserBabel],
   });
@@ -11,6 +17,7 @@ export function formatJS(value: string): string {
 
 export function formatHTML(value: string): string {
   return prettier.format(value, {
+    ...commonOptions,
     parser: "html",
     plugins: [parserHtml],
   });
