bcda4d05b1eabab3d45acf558cf926b26b60e9c4
Ryohei Ikegami <iofg2100@gmail.com>
2022-09-07 13:21:54 +0900

    Detect JPG image

--- plugin-src/util.ts
+++ plugin-src/util.ts
@@ -88,6 +88,9 @@ export function imageToDataURL(data: Uint8Array): string | undefined {
   if (data[0] === 0x89 && data[1] === 0x50 && data[2] === 0x4e) {
     const base64 = Buffer.from(data).toString("base64");
     return "data:image/png;base64," + base64;
+  } else if (data[0] === 0xff && data[1] === 0xd8 && data[2] === 0xff) {
+    const base64 = Buffer.from(data).toString("base64");
+    return "data:image/jpeg;base64," + base64;
   } else {
     console.error("TODO: unsupported image data type");
     return undefined;
